a2d1cccb085586adf941960c599f5c3547bb1f2a
Edgar F <eflores.ef@gmail.com>
2019-12-04 19:54:26 -0800

    Selection sort in progress, not working with all edge cases

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,17 +6,21 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
-
+        print(f'cur_index is {cur_index}')
+        for x in range(cur_index + 1, len(arr) - 1):
+            if arr[x] < arr[smallest_index]:   
+                smallest_index = x
+            else:
+                pass
+        
+        print(f'smallest_index is {smallest_index} with number {arr[smallest_index]}')
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
         # TO-DO: swap
 
-
-
-
+    print(smallest_index)
     return arr
 
+print(selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):

bf0b0c37222d5216c18689bdedaa238b79fbb810
Edgar F <eflores.ef@gmail.com>
2019-12-07 08:21:41 -0800

    Selection sort completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,26 +6,48 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        print(f'cur_index is {cur_index}')
-        for x in range(cur_index + 1, len(arr) - 1):
+        # print(f'cur_index is {cur_index}')
+        for x in range(cur_index, len(arr)):
             if arr[x] < arr[smallest_index]:   
                 smallest_index = x
+                # print(f'smallest_index - {smallest_index} number {arr[smallest_index]}')
             else:
                 pass
         
-        print(f'smallest_index is {smallest_index} with number {arr[smallest_index]}')
+        # print(f'smallest_index is {smallest_index} with number {arr[smallest_index]}')
         arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
         # TO-DO: swap
 
-    print(smallest_index)
+    # print(smallest_index)
     return arr
 
 print(selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
+print(selection_sort([0, 1, 2, 3, 4, 5]))
+print(selection_sort([]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    sorted = False
+
+    while not sorted:
+
+        swap = True
+
+        for i in range(0, len(arr) - 1):
+            cur_index = i
+            
+            if swap == True:
+                if arr[cur_index] > arr[cur_index + 1]:
+                    arr[cur_index], arr[cur_index + 1] = arr[cur_index + 1], arr[cur_index]
+                else:
+                    swap = False
+            else:
+                sorted = True
 
     return arr
+
+
+print(bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 
 # STRETCH: implement the Count Sort function below
